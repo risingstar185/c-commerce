85a4c33a8997818afc07ef26663281be87ef6029
ayush patel <152726075+risingstar185@users.noreply.github.com>
2025-11-03 00:48:36 +0530

    Update CORS configuration to include new origin

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,8 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
   origin:['https://shopeaseone.onrender.com',
-    'http://localhost:5174',
-    'http://localhost:5175'
+    'https://shopeaseonee.onrender.com',
+
     ],
   credentials:true
 }))
