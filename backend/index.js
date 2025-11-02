c50d4f6bca2f4ef938d95ceb86e8351aea5f61e2
ayush patel <152726075+risingstar185@users.noreply.github.com>
2025-11-03 00:45:02 +0530

    Update CORS origin to include production URL

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ let app=express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin:['http://localhost:5173',
+  origin:['https://shopeaseone.onrender.com',
     'http://localhost:5174',
     'http://localhost:5175'
     ],
